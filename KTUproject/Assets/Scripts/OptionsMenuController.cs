92f7e7be335a1fa5045cd8a98ad1b8a7659af56f
Julius Poška <julius.poska@ktu.edu>
2022-05-07 17:16:38 +0300

    Working sensitivity slider added

--- KTUproject/Assets/Scripts/OptionsMenuController.cs
+++ KTUproject/Assets/Scripts/OptionsMenuController.cs
@@ -1,28 +1,41 @@
 using System.Collections;
-using UnityEngine.UI;
 using System.Collections.Generic;
-
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
 
 public class OptionsMenuController : MonoBehaviour
 {
     public bool initialized = false;
     public Slider mouseSensitivitySlider;
+    public CinemachineFreeLook cam;
 
     void Start()
     {
-        if (PlayerPrefs.HasKey("Sensitivity")) 
+        if (PlayerPrefs.HasKey("Sensitivity") && PlayerPrefs.HasKey("SensitivityY"))
         {
-            mouseSensitivitySlider.value = PlayerPrefs.GetFloat("Sensitivity");         
+            mouseSensitivitySlider.value = PlayerPrefs.GetFloat("Sensitivity");
+            cam.m_XAxis.m_MaxSpeed = PlayerPrefs.GetFloat("Sensitivity");
+            cam.m_YAxis.m_MaxSpeed = PlayerPrefs.GetFloat("SensitivityY");
         }
         initialized = true;
     }
 
+    /// <summary>
+    /// Nustato sensitivity pagal slider'io reiksme
+    /// </summary>
+    /// <param name="val">Sensitivity value</param>
     public void SetMouseSensitivity(float val)
     {
         if (!initialized) return;
         if (!Application.isPlaying) return;
         PlayerPrefs.SetFloat("Sensitivity", val);
+        PlayerPrefs.SetFloat("SensitivityY", (val / 100));
         Debug.Log("Set sensitivity to " + val);
+        cam.m_XAxis.m_MaxSpeed = val;
+        cam.m_YAxis.m_MaxSpeed = (val / 100);
     }
 }
