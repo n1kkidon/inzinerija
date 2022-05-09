1af2f0ab2160e15ea2246e87c04f0759ae953149
Karolis Krutkevičius <>
2022-05-09 23:27:14 +0300

    Level timer added (WIP)

--- KTUproject/Assets/Scripts/GameManager.cs
+++ KTUproject/Assets/Scripts/GameManager.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public bool gameHasEnded = false;
     public float restartDelay = 1f;
     public GameObject CompleteLevelUi;
+    //public Text finishedIn;
 
     public void CompleteLevel()
     {
+        TimeController.instance.EndTimer();
+        //string finishTime = TimeController.instance.GetTime();
+        //finishedIn.text = "Finished in: "+finishTime;
         CompleteLevelUi.SetActive(true);
     }
 
